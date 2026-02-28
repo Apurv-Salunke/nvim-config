f0306c18004bf28e369f59222fa09818d0c5dbc4
Apurv-Salunke <salunke.apurv7@gmail.com>
2026-02-28 05:54:06 +0530

    Fix snacks explorer and disable netrw

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded on the VeryLazy event
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable netrw (use snacks explorer instead)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,10 +4,32 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-      -- Ensure picker is enabled
       picker = { enabled = true },
-      -- Ensure explorer is enabled
       explorer = { enabled = true },
+      indent = { enabled = true },
+      notifier = { enabled = true },
+    },
+    keys = {
+      -- Open file explorer
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      -- Open picker
+      { "<leader><space>", function() Snacks.picker.files() end, desc = "Find Files" },
+      { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    },
+  },
+  -- Disable netrw since we're using snacks explorer
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "stevearc/oil.nvim",
+        opts = {},
+        config = function()
+          vim.g.loaded_netrw = 1
+          vim.g.loaded_netrwPlugin = 1
+        end,
+      },
     },
   },
 }
